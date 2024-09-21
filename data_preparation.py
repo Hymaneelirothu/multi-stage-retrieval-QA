df75431163ba11e20bab4d3c4141044b9f2141ed
Hymaneelirothu <142670415+Hymaneelirothu@users.noreply.github.com>
2024-09-20 18:50:32 -0700

    Update data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -1,9 +1,14 @@
 from beir import util
 from beir.datasets.data_loader import GenericDataLoader
 
-def load_dataset(dataset_name="nq", num_docs=100):
-    data_path = util.download_and_unzip(f"https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{dataset_name}.zip", dataset_name)
+def load_dataset(num_docs=100):
+    # Use the fixed URL directly for the Natural Questions dataset
+    data_path = util.download_and_unzip("https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/nq.zip", "nq")
+    
+    # Load the dataset
     corpus, queries, qrels = GenericDataLoader(data_folder=data_path).load(split="test")
+    
     # Limit the size of the corpus for memory efficiency
     small_corpus = {k: corpus[k] for k in list(corpus.keys())[:num_docs]}
+    
     return small_corpus, queries, qrels
