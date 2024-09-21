27f85a7c6eb4505705afecb6e1e7e5f1835f57f0
Hymaneelirothu <142670415+Hymaneelirothu@users.noreply.github.com>
2024-09-20 18:43:39 -0700

    Update data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -1,7 +1,9 @@
 from beir import util
 from beir.datasets.data_loader import GenericDataLoader
 
-def load_dataset(dataset_name="nq"):
+def load_dataset(dataset_name="nq", num_docs=100):
     data_path = util.download_and_unzip(f"https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{dataset_name}.zip", dataset_name)
     corpus, queries, qrels = GenericDataLoader(data_folder=data_path).load(split="test")
-    return corpus, queries, qrels
+    # Limit the size of the corpus for memory efficiency
+    small_corpus = {k: corpus[k] for k in list(corpus.keys())[:num_docs]}
+    return small_corpus, queries, qrels
