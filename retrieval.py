752249ae10919e9541df570a5ff12f66d759a39c
Hymaneelirothu <142670415+Hymaneelirothu@users.noreply.github.com>
2024-09-20 18:56:58 -0700

    Update retrieval.py

--- retrieval.py
+++ retrieval.py
@@ -1,13 +1,11 @@
-from sentence_transformers import SentenceTransformer, util
-
-def load_embedding_model(model_name):
-    return SentenceTransformer(model_name)
-
 def retrieve_top_k(model, query, corpus, k=10):
+    if not corpus or len(corpus) == 0:
+        raise ValueError("Corpus is empty, cannot retrieve passages.")
+
     query_embedding = model.encode(query, convert_to_tensor=True)
-    corpus_embeddings = model.encode([corpus[doc_id]["text"] for doc_id in corpus], convert_to_tensor=True)
-    
-    hits = util.semantic_search(query_embedding, corpus_embeddings, top_k=k)[0]
-    top_k_passages = [(corpus[list(corpus.keys())[hit['corpus_id']]]["text"], hit['score']) for hit in hits]
+    corpus_embeddings = model.encode([corpus[doc_id]["text"] for doc_id in corpus])
+
+    hits = util.semantic_search(query_embedding, corpus_embeddings, top_k=k)
+    top_k_passages = [(corpus[list(corpus.keys())[hit['corpus_id']]]["text"]) for hit in hits[0]]
     
     return top_k_passages
