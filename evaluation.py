7c455271b0472dae1367c07775d867247b139a56
Hymaneelirothu <142670415+Hymaneelirothu@users.noreply.github.com>
2024-09-21 21:44:56 -0700

    Delete evaluation.py

--- evaluation.py
+++
@@ -1,5 +0,0 @@
-from sklearn.metrics import ndcg_score
-
-def evaluate_ndcg(top_k_passages, qrels):
-    relevance_scores = [1 if doc in qrels else 0 for doc, _ in top_k_passages]
-    return ndcg_score([relevance_scores], [[1]*len(relevance_scores)], k=10)
